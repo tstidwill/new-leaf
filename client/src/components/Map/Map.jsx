4ce75d47783a35eb713bf484fb30705ad867203e
tstidwill <tara.stidwill@gmail.com>
2024-06-02 12:24:21 -0400

    Conditionally render GoogleMap to the discover page, coordinates hardcoded

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,5 +1,28 @@
 import "./Map.scss";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 export default function Map() {
-  return <div className="map"></div>;
+  const center = {
+    lat: 45.425,
+    lng: -75.695,
+  };
+  //starting location currently Ottawa, will become dynamic on user input
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (loadError) {
+    return <div>Error loading map</div>;
+  }
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerClassName="map-container"
+      center={center}
+      zoom={10}
+    ></GoogleMap>
+  ) : (
+    <div className="map-container">Loading...</div>
+  );
 }
