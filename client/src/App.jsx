200a572f2899f25555a0f26907d095f6fb5cb13b
tstidwill <tara.stidwill@gmail.com>
2024-06-03 15:13:19 -0400

    Move postalcode up one node to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,31 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import DiscoverPage from "./pages/DiscoverPage/DiscoverPage";
+import { useState } from "react";
 
 function App() {
+  const [postalCode, setPostalCode] = useState("");
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/discover" element={<DiscoverPage />} />
+        <Route
+          path="/"
+          element={
+            <LandingPage
+              postalCode={postalCode}
+              setPostalCode={setPostalCode}
+            />
+          }
+        />
+        <Route
+          path="/discover"
+          element={
+            <DiscoverPage
+              postalCode={postalCode}
+              setPostalCode={setPostalCode}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
